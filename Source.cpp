95d99f321f3050b2a6a74e7274ed88cdef34d84a
Wiktor Bukala <vigor_pictures@o2.pl>
2018-07-24 22:22:29 +0200

    operators done

--- Source.cpp
+++ Source.cpp
@@ -6,13 +6,13 @@ int main()
 {
 
 	se to1;
-	to1 = "test ";
+	to1 = "test";
 	se to2 = "test string1";
-	se to3 = "blabla";
-	se to4 = to1 + to2 + to3;
+	se to3 = "blabla ";
+	se to4 = to1 + " dupa " + to2 + to3 + "_error";
 	to1.showarr(to1.strarr);
 	to2.showarr(to2.strarr);
-	to2.showarr(to4.strarr);
+	to4.showarr(to4.strarr);
 	std::cout << "\n";
 	system("pause");
 	return 0;
